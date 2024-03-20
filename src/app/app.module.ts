bb393d57e9b812075acf94f8b582092bccc6fc56
Francilvan Lima <francilvanlima@gmail.com>
2024-03-20 11:04:22 -0400

    Create: Header e Menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,41 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ServicosComponent } from './components/servicos/servicos.component';
+import { ReservaComponent } from './components/reserva/reserva.component';
+import { DepoimentosComponent } from './components/depoimentos/depoimentos.component';
+import { CardapioComponent } from './components/cardapio/cardapio.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    HeaderComponent,
+    FooterComponent,
+    ServicosComponent,
+    ReservaComponent,
+    DepoimentosComponent,
+    CardapioComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
